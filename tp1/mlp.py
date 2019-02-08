gradients are computed out of train(). fixed bug with softmax.

tp1/mlp.py
@@ -38,39 +38,63 @@ class NN:
     def activation(self, x):
         return np.tanh(x)
 
-    def loss(self,prediction, true_labels):
-        data_size = true_labels.size
-        logprobs = -np.log(prediction[range(data_size),true_labels])
-        the_loss = logprobs.mean()
+    def loss(self,X, true_labels):
+        data_size = X.shape[0]
+        pred = self.forward(X)
+        logprobs = -np.log(pred[range(data_size),true_labels])
+
+        return logprobs.mean()
 
     def softmax(self,input):
         num = np.exp(input)
-        prob = num / np.sum(num, axis=0, keepdims=True)
+        prob = num / np.sum(num, axis=1, keepdims=True)
         
         return prob
 
 
-    def backward(self,cache,labels):
-        pass
+    def grads(self,X, pred, y, cache):
+        n_X = pred.shape[0]
+        y_yhat = pred
+        y_yhat[range(n_X),y] -= 1
+        dw2 = (cache.T).dot(y_yhat)
+        db2 = np.sum(y_yhat, axis=0, keepdims=True)
+        term_2 = y_yhat.dot(dw2.T) * (1 - np.power(cache,2))
+        dw1 = np.dot(X.T, term_2)
+        db1 = np.sum(term_2, axis=0)
+        return (dw2,db2,dw1,db1)
 
-    def update(self,grads):
-        w1 += -self.lr * grads['w1']
-        b1 += -self.lr * grads['b1']
-        w2 += -self.lr * grads['w2']
-        b2 += -self.lr * grads['b2']
-
-
-    def train(self, train_set, va_set, lr, batch_size=100):
+    def train(self, train_set, va_set, lr=0.01, batch_size=100, n_epochs=10):
         train_data = train_set[0]
         train_labels = train_set[1]
 
-        # SGD
-        for i in range(0, train_data.shape[0], batch_size):
-            print(i, "--", i+batch_size)
+        for epoch in range(n_epochs):
+            print("Epoch " , (epoch + 1))
+            # SGD
+            for i in range(0, train_data.shape[0], batch_size):
+                # Select batch.
+                X = train_data[i:i + batch_size]
+                y = train_labels[i:i + batch_size]
+                n_X = X.shape[0]
 
+                # Fwd prop.
+                # We could have called forward() but let's save a function call !
+                z1 = X.dot(self.w1) + self.b1
+                a1 = self.activation(z1)
+                z2 = a1.dot(self.w2) + self.b2
+                pred = self.softmax(z2)
+
+                # Back prop
+                dw2,db2,dw1,db1 = self.grads(X, pred, y, a1)
+
+                # Update params
+                self.w1 -= lr * dw1
+                self.b1 -= lr * db1
+                self.w2 -= lr * dw2
+                self.b2 -= lr * db2
+            print("Loss=", self.loss(va_set[0], va_set[1]) )
 
     def test(self, test_set):
-        pass
+        print("Final Test Loss=", self.loss(test_set[0], test_set[1]) )
 
 if __name__ == '__main__':
     import argparse
@@ -78,8 +102,10 @@ if __name__ == '__main__':
     parser.add_argument('--datafile', type=str, default=None, help='path to .npy data')
 
     args = parser.parse_args()
-    tr, va, te = np.load(args.datafile)
+    train, valid, test = np.load(args.datafile)
     
-    mlp = NN(hidden_dim= 1024, in_dim=784)
+    mlp = NN(hidden_dim= 200, in_dim=784)
     mlp.initialize_weights(init_type = 'gauss')
-    mlp.train(tr, va, 0.01)
+    mlp.train(train, valid, 0.001, batch_size=200, n_epochs=10)
+    mlp.test(test)
+    #np.save('model.npy',(mlp.w1, mlp.b1, mlp.w2, mlp.b2))