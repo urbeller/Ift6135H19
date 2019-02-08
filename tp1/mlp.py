350cb607f30e4371a34a7424f30958536f5de331
Jamil Drareni <urbeller@macbookpro>
2019-02-08 10:36:34 -0500

    simple batch_size loop. implemented the loss function.

--- tp1/mlp.py
+++ tp1/mlp.py
@@ -3,27 +3,25 @@
 import numpy as np
 
 class NN:
-    def __init__(self,in_dim=784, hidden_dim=1024, out_dim=10, init_type='zero'):
-        self.init_type = init_type
+    def __init__(self,in_dim=784, hidden_dim=1024, out_dim=10):
         self.w1 = None
         self.w2 = None
         self.b1 = None
         self.b2 = None
-        self.a1 = None # caches a1 for backprop.
         self.input_dim = in_dim
         self.hidden_dim = hidden_dim
         self.output_dim = out_dim
-        self.initialize_weights()
 
-    def initialize_weights(self):
-        self.b1 = np.zeros((1, self.input_dim))
+
+    def initialize_weights(self, init_type='zero'):
+        self.b1 = np.zeros((1, self.hidden_dim))
         self.b2 = np.zeros((1, self.output_dim))
     
-        if self.init_type == 'gauss':
+        if init_type == 'gauss':
             self.w1 = np.random.randn(self.input_dim, self.hidden_dim)
             self.w2 = np.random.randn(self.hidden_dim, self.output_dim)
 
-        elif self.init_type == 'glorot':
+        elif init_type == 'glorot':
             pass
         else:
             self.w1 = np.zeros((self.input_dim, self.hidden_dim))
@@ -31,16 +29,19 @@ class NN:
 
     def forward(self,input):
         z1 = input.dot(self.w1) + self.b1
-        self.a1 = self.activation(z1)
-        output = self.a1.dot(self.w2) + b2
+        a1 = self.activation(z1)
+        output = a1.dot(self.w2) + self.b2
         out_probs = self.softmax(output)
 
         return out_probs
 
-    def activation(self,input): lambda x: np.tanh(x)
+    def activation(self, x):
+        return np.tanh(x)
 
     def loss(self,prediction, true_labels):
-        pass
+        data_size = true_labels.size
+        logprobs = -np.log(prediction[range(data_size),true_labels])
+        the_loss = logprobs.mean()
 
     def softmax(self,input):
         num = np.exp(input)
@@ -59,8 +60,14 @@ class NN:
         b2 += -self.lr * grads['b2']
 
 
-    def train(self, train_set, va_set, lr):
-        pass
+    def train(self, train_set, va_set, lr, batch_size=100):
+        train_data = train_set[0]
+        train_labels = train_set[1]
+
+        # SGD
+        for i in range(0, train_data.shape[0], batch_size):
+            print(i, "--", i+batch_size)
+
 
     def test(self, test_set):
         pass
@@ -71,8 +78,8 @@ if __name__ == '__main__':
     parser.add_argument('--datafile', type=str, default=None, help='path to .npy data')
 
     args = parser.parse_args()
-    _tr, _va, _te = np.load(args.datafile)
-    tr=_tr[0]; va=_va[0]; te=_te[0]
+    tr, va, te = np.load(args.datafile)
     
-    mlp = NN(hidden_dim= 1024, init_type='gauss')
-    print(mlp.w1)
+    mlp = NN(hidden_dim= 1024, in_dim=784)
+    mlp.initialize_weights(init_type = 'gauss')
+    mlp.train(tr, va, 0.01)
