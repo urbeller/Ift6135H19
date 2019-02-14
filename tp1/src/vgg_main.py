ea9c5b1690ccc36138c8349459effe36c86b2954
Jamil Drareni <urbeller@macbookpro>
2019-02-14 10:51:40 -0500

    Added data augmentation. args parsing. validation loss function.

--- tp1/src/vgg_main.py
+++ tp1/src/vgg_main.py
@@ -21,27 +21,70 @@ def split_data(data_loader, valid_prop=0.1, bs=64):
     val_sampler = SubsetRandomSampler(valid_idx)
 
     
-    train_loader = DataLoader(data_loader.dataset, batch_size=bs, sampler=tr_sampler)
-    valid_loader = DataLoader(data_loader.dataset, batch_size=bs, sampler=val_sampler)
+    train_loader = DataLoader(data_loader.dataset, batch_size=bs, sampler=tr_sampler, drop_last=True)
+    valid_loader = DataLoader(data_loader.dataset, batch_size=bs, sampler=val_sampler, drop_last=True)
     
     return (train_loader, valid_loader)
 
 
-def load_images(image_size=32, batch_size=64, root="../data/cat_dog/trainset"):
+def load_images(batch_size=64, root="../data/cat_dog/trainset"):
 
     transform = transforms.Compose([
-        transforms.Resize(image_size),
+        transforms.RandomRotation(15),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
-    train_set = datasets.ImageFolder(root=root, transform=transform)
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2)
+    data_set = datasets.ImageFolder(root=root, transform=transform)
+    data_loader = torch.utils.data.DataLoader(data_set, batch_size=batch_size, shuffle=True, num_workers=2)
 
-    return train_loader  
+    return data_loader  
+
+
+def train(model, train_loader, optimizer, epoch):
+    model.train()
+
+    # SGD iteration
+    total_loss = 0
+    n_data = 0 
+    for idx, (X, Y) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(X)
+        loss = F.nll_loss(output, Y, reduction='sum')
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item() * X.size(0)
+        n_data += X.size(0)
+
+        if idx % 30 == 0:
+            print("Epoch = ",epoch, "\titeration : ", idx + 1, "/", len(train_loader), "\tLoss = ", loss.item())
+
+    return total_loss / n_data
+
+
+def validate(model, valid_loader):
+
+    loss = 0
+    n_data = 0
+    model.eval()
+    with torch.no_grad():
+        for idx, (X, Y) in enumerate(valid_loader):
+            output = model(X)
+            loss += F.nll_loss(output, Y).item() * X.size(0)
+            n_data += X.size(0)
+
+        return loss / n_data
 
 
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', type=int, default=64, help='train mini-batch size')
+    parser.add_argument('--epochs', type=int, default=10, help='number of epochs')
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+
+    args = parser.parse_args()
 
     data_loader=load_images(root="../data/cat_dog/trainset")
     test_loader=load_images(root="../data/cat_dog/testset")
@@ -49,3 +92,10 @@ if __name__ == '__main__':
     tr_loader, val_loader = split_data(data_loader, valid_prop = 0.1, bs=64)
 
     model = Vgg(num_classes=2)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr)
+
+    for epoch in range(args.epochs):
+        train_loss = train(model, tr_loader, optimizer, epoch + 1)
+        valid_loss = validate(model, val_loader)
+
+
