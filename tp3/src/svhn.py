d79428f2ccc964ee0afd19bed98a5c0ae534a7cb
Jamil Drareni <urbeller@macbookpro>
2019-04-16 05:31:37 -0400

    modify kl and reparam

--- tp3/src/svhn.py
+++ tp3/src/svhn.py
@@ -41,12 +41,11 @@ def train(device, model, train_loader, epochs=100):
       # Compute loss
       scaling_fact = X.shape[0] * X.shape[1] * X.shape[2] * X.shape[3]
 
-      bce = latent_loss(recons, X)
+      bce = latent_loss(recons, X, reduction="none")
       variance = torch.exp(logvar)
       variance_sq = variance * variance
-      #kl = 0.5 * torch.mean(mu * mu + variance_sq - torch.log(variance_sq) - 1)
       kl = -0.5 * torch.sum(1 + logvar - mu**2 - torch.exp(logvar))
-      kl /= scaling_fact
+      #kl /= scaling_fact
 
       loss = bce + kl
       loss.backward()
