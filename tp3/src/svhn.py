f2520eb37b8a2c3dfb94eb9b2bce3b3898cd1d07
Jamil Drareni <urbeller@macbookpro>
2019-04-16 05:33:04 -0400

    modify kl and reparam

--- tp3/src/svhn.py
+++ tp3/src/svhn.py
@@ -18,9 +18,9 @@ import utils
 def train(device, model, train_loader, epochs=100):
   
   if device == 'cuda':
-    latent_loss = nn.BCELoss().cuda()
+    latent_loss = nn.BCELoss( reduction="none").cuda()
   else:
-    latent_loss = nn.BCELoss()
+    latent_loss = nn.BCELoss( reduction="none")
 
   utils.initialize_weights(model)
   model.train()
@@ -41,7 +41,7 @@ def train(device, model, train_loader, epochs=100):
       # Compute loss
       scaling_fact = X.shape[0] * X.shape[1] * X.shape[2] * X.shape[3]
 
-      bce = latent_loss(recons, X, reduction="none")
+      bce = latent_loss(recons, X)
       variance = torch.exp(logvar)
       variance_sq = variance * variance
       kl = -0.5 * torch.sum(1 + logvar - mu**2 - torch.exp(logvar))
