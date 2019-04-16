eedbdce5b00d1247a5947c636301ca69a6dd0d55
Jamil Drareni <urbeller@macbookpro>
2019-04-16 12:17:43 -0400

    print losses

--- tp3/src/svhn.py
+++ tp3/src/svhn.py
@@ -44,7 +44,7 @@ def train(device, model, train_loader, epochs=100):
       bce = latent_loss(recons, X)
       kl = -0.5 * torch.sum(1 + logvar - mu**2 - torch.exp(logvar))
       kl /= scaling_fact
-			print("\t bce=", bce, "kl=",kl)
+      print("\t bce=", bce, "kl=",kl)
       loss = bce +  kl
       loss.backward()
       train_loss += loss.item()
